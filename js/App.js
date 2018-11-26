fc3a9351985bd5e75ce8758b07386a20a43abf91
goujonpa <goujonpa@gmail.com>
2018-11-26 17:41:03 +0100

    genesis

---
+++ js/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(...args) {
+        super(...args);
+    }
+
+    render() {
+        return <h1>BJR</h1>;
+    }
+}
+
+export default App;
